b49ca188ac909c852062983a182599c1d6d71069
SISparang <goflvhxj2547@gmail.com>
2018-05-21 15:47:08 +0900

    Add files via upload

--- site.js
+++ site.js
@@ -2,8 +2,9 @@ function siteOpen(){
             var Url = "site.html";	//팝업창에 출력될 페이지 URL
             var screenW = screen.availWidth;
             var screenH = screen.availHeight;
-            var posL=(screenW-784) / 2;
-            var Option = "width=715, height=980, left="+posL+", resizable=no, scrollbars=no, status=no;";    //팝업창 옵션(optoin)
+            var posL=(screenW-715) / 2;
+            var posT=(screenH-730) / 2;
+            var Option = "width=715, height=730, top="+posT+", left="+posL+", resizable=no, scrollbars=no, status=no;";    //팝업창 옵션(optoin)
 
             window.open(Url,"",Option);
         }
