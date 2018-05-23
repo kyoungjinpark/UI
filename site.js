f948bc3edcb0ca82174868fc2c2b4705aeb853c4
kyoungjinpark <38056709+kyoungjinpark@users.noreply.github.com>
2018-05-23 15:49:06 +0900

    Add files via upload

--- site.js
+++ site.js
@@ -8,3 +8,14 @@ function siteOpen(){
 
             window.open(Url,"",Option);
         }
+
+function loginOpen(){
+            var Url = "login.html";	//팝업창에 출력될 페이지 URL
+            var screenW = screen.availWidth;
+            var screenH = screen.availHeight;
+            var posL=(screenW-715) / 2;
+            var posT=(screenH-730) / 2;
+            var Option = "width=715, height=430, top="+posT+", left="+posL+", resizable=no, scrollbars=no, status=no;";    //팝업창 옵션(optoin)
+
+            window.open(Url,"",Option);
+        }
