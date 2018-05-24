73639960d75d82f764cc19917b99c1954fa4b194
kyoungjinpark <38056709+kyoungjinpark@users.noreply.github.com>
2018-05-24 10:55:08 +0900

    Update site.js

--- site.js
+++ site.js
@@ -15,7 +15,7 @@ function loginOpen(){
             var screenH = screen.availHeight;
             var posL=(screenW-715) / 2;
             var posT=(screenH-730) / 2;
-            var Option = "width=715, height=430, top="+posT+", left="+posL+", resizable=no, scrollbars=no, status=no;";    //팝업창 옵션(optoin)
+            var Option = "width=615, height=700, top="+posT+", left="+posL+", resizable=no, scrollbars=no, status=no;";    //팝업창 옵션(optoin)
 
             window.open(Url,"",Option);
         }
